95794e9ca816d63b12b70c75a565327a68acd607
Michael Ketelaar <mketelaar@me.com>
2021-05-14 09:08:17 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Michael Ketelaar on 14.05.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
